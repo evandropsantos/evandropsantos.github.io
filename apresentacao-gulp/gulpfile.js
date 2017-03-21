ccd88dd0232f585ae078d3716accff53b79bffa7
Evandro P. Santos <evandro.santos@multilaser.com.br>
2017-03-21 10:36:51 -0300

    Estrutura Apresentacao Automatizacao front-end com GULP

---
+++ apresentacao-gulp/gulpfile.js
@@ -0,0 +1,79 @@
+var gulp = require('gulp'),
+    del = require('del'),
+    imagemin = require('gulp-imagemin'),
+
+    sass = require('gulp-sass'),
+    sourcemaps = require('gulp-sourcemaps'),
+    cleanCSS = require('gulp-clean-css'),
+    
+    jshint = require('gulp-jshint'),
+    stylish = require('jshint-stylish');
+    stripDebug = require('gulp-strip-debug'),
+    uglify = require('gulp-uglify'),
+    concat = require('gulp-concat');
+
+gulp.task('clean:build', function() {
+
+    return del('build/*');
+});
+
+gulp.task('minify-images', function() {
+
+    gulp.src('sources/images/*.+(png|jpg|jpeg|gif|svg|ico)')
+    .pipe(imagemin({
+        progressive: true,
+        optimizationLevel: 3,
+        interlaced: true
+    }))
+    .pipe(gulp.dest('build/images'));
+});
+
+/* Styles */
+gulp.task('generate-sass', function() {
+    
+    return gulp.src('sources/styles/**/*.scss')
+        .pipe( sourcemaps.init() )
+        .pipe( sass().on('error', sass.logError) )
+        .pipe(sourcemaps.write('/maps'))
+        .pipe(gulp.dest('build/styles'))
+});
+
+gulp.task('minify-styles', ['generate-sass'], function() {
+
+    gulp.src('build/styles/*.css')
+    .pipe(cleanCSS({ compatibility: 'ie8'}) )
+    .pipe(gulp.dest('build/styles'));
+});
+
+/* Scripts */
+gulp.task('minify-script', function() {
+
+    gulp.src('sources/scripts/**/*.js')
+    .pipe(stripDebug())
+    .pipe(concat('common.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('build/scripts'))
+});
+
+/*----------  Desenvolvimento  ----------*/
+gulp.task('default', function() {
+    
+    gulp.watch('sources/images/*.+(png|jpg|jpeg|gif|svg|ico)', ['minify-images']);
+    gulp.watch('sources/styles/**/*.scss', ['generate-sass']);
+
+    gulp.watch('sources/scripts/**/*.js').on('change', function(event) {
+        
+        console.log("Linting " + event.path);
+
+        gulp.src(event.path)
+            .pipe(jshint())
+            .pipe(jshint.reporter(stylish))
+            .pipe(gulp.dest('build/scripts'));
+    });
+});
+
+/*----------  Producao  ----------*/
+gulp.task('build', ['clean:build'], function() {
+
+    gulp.start('minify-styles', 'minify-script', 'minify-images');
+});
---
+++ apresentacao-gulp/sources/scripts/common.js
@@ -0,0 +1,5 @@
+function valor(a, b) {
+    return a + b;
+}
+
+console.log( valor(2,5) );
