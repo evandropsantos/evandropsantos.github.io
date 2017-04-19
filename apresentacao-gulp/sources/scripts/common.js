4783e0db12e0fac3fb0a439455e0ce70691e01f8
Evandro P. Santos <evandropsantos@gmail.com>
2017-04-18 21:18:57 -0300

    Apresentacao power point

--- apresentacao-gulp/sources/scripts/common.js
+++ apresentacao-gulp/sources/scripts/common.js
@@ -12,6 +12,7 @@ Common = function() {
 
         $('a[href*="#"]:not([href="#"])').click(function() {
             
+            
             if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
                 
                 var target = $(this.hash);
